7bdc3c73f14ca263333641503a00db9d295bbfe8
samrat lamichhane <samratlamichhane46@gmail.com>
2026-09-08 08:13:18 +0545

    Week2

--- app1/models.py
+++ app1/models.py
@@ -13,3 +13,10 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    class Category(models.Model):
+        name =models.CharField(max_length=100)
+        description = models.TextField
+
+        def __str__(self):
+            return self.name
